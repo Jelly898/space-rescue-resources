aa6431c2cae839e41441f17908a1a2305d2522ce
Jelly898 <139197322+Jelly898@users.noreply.github.com>
2023-07-20 12:32:56 +1000

    Created welcome screen

---
+++ Objects/Title.py
@@ -0,0 +1,12 @@
+from GameFrame import RoomObject
+
+class Title(RoomObject):
+
+
+
+    def __init__(self, room, x, y):
+        RoomObject.__init__(self, room, x, y)
+
+        #Set image
+        image = self.load_image("Title.png")
+        self.set_image(image,800,350)
